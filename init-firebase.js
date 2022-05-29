eff05b763e8df1a55a26ef5f5ad6859836ecfc8e
Saif Al-Jilani <jilanisaif2@gmail.com>
2022-05-29 11:15:10 +0300

    Create init-firebase.js

---
+++ init-firebase.js
@@ -0,0 +1,17 @@
+var firebaseConfig = {
+    apiKey: "Your API KEY",
+    authDomain: "",
+    projectId: "Your Project Id",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+
